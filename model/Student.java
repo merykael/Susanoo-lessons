64f36941f54527338f8c196c53bbe851c115ea9f
mery_kael <mery.kael@gmail.com>
2011-09-09 00:35:51 +0300

    create class "student"

---
+++ model/Student.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Student {
+	private String first_name, second_name, father_name;
+
+	public Student(String first_name, String second_name, String father_name) {
+		this.first_name = first_name;
+		this.second_name = second_name;
+		this.second_name = father_name;
+	}
+	
+	public String getFirstName() {
+		return first_name;
+	}
+	
+	public String getSecondName() {
+		return second_name;
+	}
+	
+	public String getFatherName() {
+		return father_name;
+	}
+	
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+	
+	public void setSecondName(String second_name) {
+		this.second_name = second_name;
+	}
+	
+	public void setFatherName(String father_name) {
+		this.father_name = father_name;
+	}
+}
+
