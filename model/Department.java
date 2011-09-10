2319d19e7512d385f0fc7f8f7b87c9684a32ff13
mery_kael <mery.kael@gmail.com>
2011-09-10 20:17:43 +0300

    added "Groups" to class "Department"

--- model/Department.java
+++ model/Department.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.util.HashSet;
+
 public class Department {
 	private String name, abbreviature;
+	private HashSet<Group> groups;
 
 	public Department(String name, String abbreviature) {
 		this.name = name;
 		this.abbreviature = abbreviature;
+		this.groups = new HashSet<Group>();
 	}
 
 	public String getName() {
@@ -23,4 +27,9 @@ public class Department {
 	public void setAbbreviature(String abbreviature) {
 		this.abbreviature = abbreviature;
 	}
+
+	public HashSet<Group> getGroups() {
+		return groups;
+	}
+
 }
